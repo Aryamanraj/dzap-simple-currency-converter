13857fc9ff945232fa32a34fb8b39b4cbe5071b9
Aryamanraj <20cs3011@rgipt.ac.in>
2024-01-02 19:37:56 +0530

    react componenets

---
+++ web-client/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./header.css";
+//import { useNavigate } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <>
+      <div className="header">{/* <img src={logo}></img> */}</div>
+      <div className="line"> </div>
+    </>
+  );
+};
+
+export default Header;
