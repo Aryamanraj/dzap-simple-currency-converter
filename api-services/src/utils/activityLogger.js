df600b97e8516c1dc8b1214558b5c34fb3a0e789
Aryamanraj <20cs3011@rgipt.ac.in>
2024-01-02 19:40:25 +0530

    api logger utilities

---
+++ api-services/src/utils/activityLogger.js
@@ -0,0 +1,40 @@
+const winston = require('winston');
+const path = require('path');
+
+const options = {
+  file: {
+    level: 'info',
+    filename: path.join(__dirname, 'logs/app.log'),
+    handleExceptions: true,
+    json: true,
+    maxsize: 5242880, // 5MB
+    maxFiles: 5,
+    colorize: false,
+  },
+  console: {
+    level: 'debug',
+    handleExceptions: true,
+    json: false,
+    colorize: true,
+  },
+};
+
+const logger = winston.createLogger({
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.json()
+  ),
+  transports: [
+    new winston.transports.File(options.file),
+    new winston.transports.Console(options.console)
+  ],
+  exitOnError: false, // do not exit on handled exceptions
+});
+
+logger.stream = {
+  write: function(message, encoding) {
+    logger.info(message);
+  },
+};
+
+module.exports = logger;
