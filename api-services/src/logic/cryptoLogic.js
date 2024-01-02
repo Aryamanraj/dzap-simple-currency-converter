4fb47a6a39d06597a921e4bb3af72ad509d362d9
Aryamanraj <20cs3011@rgipt.ac.in>
2024-01-02 19:40:35 +0530

    api main loggic

---
+++ api-services/src/logic/cryptoLogic.js
@@ -0,0 +1,73 @@
+const axios = require('axios');
+const NodeCache = require('node-cache');
+const logger = require('../utils/activityLogger');
+
+const cache = new NodeCache({ stdTTL: 1800, checkperiod: 2400 });
+
+const fetchSupportedCurrencies = async () => {
+  const cacheKey = 'supportedCurrencies';
+  const cachedData = cache.get(cacheKey);
+
+  if (cachedData) {
+    logger.info("Fetching supported currencies from cache");
+    return cachedData;
+  } else {
+    try {
+      const response = await axios.get('https://api.coingecko.com/api/v3/simple/supported_vs_currencies');
+      const data = {
+        crypto: response.data.slice(0, 11),
+        fiat: response.data.slice(11, 57),
+        others: response.data.slice(57)
+      };
+      cache.set(cacheKey, data);
+      logger.info("Supported currencies fetched from API and cached");
+      return data;
+    } catch (error) {
+      logger.error('Error fetching supported currencies: ' + error.message);
+      throw error;
+    }
+  }
+};
+
+const fetchMarketData = async () => {
+  const cacheKey = 'marketData';
+  const cachedData = cache.get(cacheKey);
+
+  if (cachedData) {
+    logger.info("Fetching market data from cache");
+    return cachedData;
+  } else {
+    try {
+      const response = await axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false');
+      const data = response.data.map(coin => ({
+        id: coin.id,
+        image: coin.image,
+        name: coin.name,
+        symbol: coin.symbol
+      }));
+      cache.set(cacheKey, data);
+      logger.info("Market data fetched from API and cached");
+      return data;
+    } catch (error) {
+      logger.error('Error fetching market data: ' + error.message);
+      throw error;
+    }
+  }
+};
+
+const computePrice = async (currency, supported, amount) => {
+  try {
+    const response = await axios.get(`https://api.coingecko.com/api/v3/simple/price?ids=${currency.toLowerCase()}&vs_currencies=${supported.toLowerCase()}`);
+    logger.info(response)
+    const curr = response.data[currency.toLowerCase()]; 
+    let price = curr[supported.toLowerCase()];
+    const total = Number(price) * Number(amount);
+    logger.info(`Price calculated for ${currency} to ${supported}, Amount: ${amount}`);
+    return { currentPriceData: price, total: total };
+  } catch (error) {
+    logger.error('Error fetching price data: ' + error.message);
+    throw error;
+  }
+};
+
+module.exports = { fetchSupportedCurrencies, fetchMarketData, computePrice };
